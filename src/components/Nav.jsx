eeae9701b7635841d162bfea52b849c1e9c20c89
Aman-web-dev <amanzhx1234+github@gmail.com>
2024-02-06 13:11:54 +0530

    Working Navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -35,7 +35,7 @@ const Nav = () => {
 
 
 
-                        <button onClick={()=>{setNavShow(!navShow)}}  type="button" className="inline-flex items-center p-2 w-10 h-8 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-default" aria-expanded="false">
+                         <button onClick={()=>{setNavShow(!navShow)}}  type="button" className="inline-flex items-center p-2 w-10 h-8 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-default" aria-expanded="false">
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
@@ -43,11 +43,14 @@ const Nav = () => {
                         </button>
 
 
-                        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                            {console.log(navShow)}
+                    
 
 
-                            {navShow ? <ul className={`font-medium flex flex-col md:flex-row p-4 md:p-0 md:items-center mt-4 border  md:space-x-8 md:mt-0 md:border-0`}>
+                        {navShow ? <div className=" w-full md:block md:w-auto" id="navbar-default">
+                      
+
+
+                             <ul className={`font-medium flex flex-col md:flex-row p-4 md:p-0 md:items-center mt-4 border  md:space-x-8 md:mt-0 md:border-0`}>
 
                                 <li>
                                     <a href="/" className="block px-4 py-2 font-bold text-black  hover:bg-gray-100 ">Home</a>
@@ -130,8 +133,8 @@ const Nav = () => {
                                     </div>
 
                                 </li>
-                            </ul>:""}
-                        </div>
+                            </ul>
+                        </div>:""}
 
 
 
